f85f3c64c7777eeaf1c2846f7ad6ab928590ef50
SiqiWan <704252212@qq.com>
2024-04-13 17:55:22 -0400

    implement index-photos

--- index-photos/lambda_function.py
+++ index-photos/lambda_function.py
@@ -1,8 +1,61 @@
+import boto3
 import json
+import requests
+
+esaccount = ("ycc","YccWsq2024!")
 
 def lambda_handler(event, context):
     # TODO implement
+    for record in event['Records']:
+        bucketName = record['s3']['bucket']['name']
+        objKey = record['s3']['object']['key']
+
+        labels = getDetectLabels(bucketName, objKey) + getCustomLabels(bucketName, objKey)
+        esObj = {
+            "objectKey": objKey,
+            "bucket": bucketName,
+            "createdTimestamp": record["eventTime"],
+            "labels": labels
+        }
+        jsonObj = json.dump(esObj)
+        
+        url = "https://search-photos-vxlnrbcnhsji3zk6gcylzym3pa.us-east-1.es.amazonaws.com"
+        response = requests.put(url, auth=esaccount, data=jsonObj, headers={'Content-Type': 'application/json'})
+        print(response)
+
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from index-photos!')
     }
+
+def getDetectLabels(bucketName, objKey):
+    rekognition_client = boto3.client('rekognition', region_name='us-east-1')
+
+    # Call Rekognition to detect labels in the image
+    response = rekognition_client.detect_labels(
+        Image={
+            'S3Object': {
+                'Bucket': bucketName,
+                'Name': objKey,
+            }
+        },
+        MaxLabels=10,  # Maximum number of labels to return
+        MinConfidence=70  # Minimum confidence level for detected labels
+    )
+
+    # Print the detected labels
+    print('Detected labels:')
+    for label in response['Labels']:
+        print(f'{label["Name"]} - {label["Confidence"]:.2f}%') 
+
+    return response['Labels']
+
+def getCustomLabels(bucketName, Objkey):
+    # Initialize the S3 client
+    s3_client = boto3.client('s3')
+    response = s3_client.head_object(Bucket=bucketName, Key=Objkey)
+    labels = response['x-amz-meta-customLabels']
+    print(labels)
+    if len(labels) == 0: 
+        return []
+    return labels
